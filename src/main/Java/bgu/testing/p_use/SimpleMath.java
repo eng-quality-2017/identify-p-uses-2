4d541fc2f39e5b268a678b05f2ea18d801eb3da9
dork <dork@wix.com>
2017-12-19 17:10:15 +0200

    update simpleMath to the right package

--- src/main/Java/bgu/testing/p_use/SimpleMath.java
+++ src/main/Java/bgu/testing/p_use/SimpleMath.java
@@ -1,6 +1,10 @@
-package bgu.testing.p_use.;
+package bgu.testing.p_use;
 
 public class SimpleMath {
+
+    public SimpleMath() {
+    }
+
     public int multiplyBy2(int i) {
         int result = i << 1;
         System.out.println(i + " multiplied by 2 is " + result);
