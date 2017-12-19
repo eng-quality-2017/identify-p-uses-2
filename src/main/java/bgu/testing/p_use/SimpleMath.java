050a2c31f402f321e7a0345a6c000044f50ce15c
Aviram Dayan <aviram26@gmail.com>
2017-12-19 17:51:09 +0200

    update simpleMath to the right package

---
+++ src/main/java/bgu/testing/p_use/SimpleMath.java
@@ -0,0 +1,13 @@
+package bgu.testing.p_use;
+
+public class SimpleMath {
+
+    public SimpleMath() {
+    }
+
+    public int multiplyBy2(int i) {
+        int result = i << 1;
+        System.out.println(i + " multiplied by 2 is " + result);
+        return result;
+    }
+}
